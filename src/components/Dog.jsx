7ff05872fe6c16b3d68b6ebb14699e9f966d3cfb
varunwebcodes <varunnegiweb>
2026-01-08 21:48:30 +0530

    implement a gsap and scrolltrigger animztion

--- src/components/Dog.jsx
+++ src/components/Dog.jsx
@@ -1,9 +1,17 @@
-import React from 'react'
+import React, { useEffect , useRef} from 'react'
 import * as THREE from "three"
 import { useThree } from '@react-three/fiber'
-import { OrbitControls , useGLTF , useTexture} from '@react-three/drei'
+import { OrbitControls , useGLTF , useTexture , useAnimations} from '@react-three/drei'
+import gsap from 'gsap'
+import { useGSAP } from '@gsap/react'
+import ScrollTrigger from 'gsap/ScrollTrigger'
 
 const Dog = () => { 
+
+
+    gsap.registerPlugin(useGSAP())
+    gsap.registerPlugin(ScrollTrigger)
+
 
     const model = useGLTF("/models/dog.drc.glb")
 
@@ -13,38 +21,83 @@ const Dog = () => {
         gl.outputColorSpace = THREE.SRGBColorSpace
     })
 
-    // const textures = useTexture({
-    //     normalMap:"/dog_normals.jpg",
-    //     sampleMatCap:"/matcap/mat-2.png"
-    // },(texture)=>{
-    //     texture.flipY = false
-    //     texture.colorSpace = THREE.SRGBColorSpace
-    // })
+    const { actions } = useAnimations(model.animations,model.scene)
+    useEffect(()=>{
+        actions["Take 001"].play()
+    }, [actions])
 
-    const [
-        normalMap,
-        sampleMatCap
-    ] = useTexture([ "/dog_normals.jpg","/matcap/mat-2.png" ])
 
-    textures.normalMap.flipY = false
-    textures.sampleMatCap.colorSpace = THREE.SRGBColorSpace
+    const [ normalMap, sampleMatCap ] = (useTexture([ "/dog_normals.jpg","/matcap/mat-2.png"]))
+    .map(texture =>{
+        texture.flipY = false
+        texture.colorSpace = THREE.SRGBColorSpace
+        return texture
+    })
+
+    const [ branchMap , branchNormalMap] = (useTexture(["/branches_diffuse.jpg", "/branches_normals.jpg" ]))
+    .map(texture =>{
+        texture.colorSpace = THREE.SRGBColorSpace
+        return texture
+    })
+
+    const dogMaterial = new THREE.MeshMatcapMaterial({
+        normalMap: normalMap,
+        matcap: sampleMatCap
+    })
+
+    const branchMaterial = new THREE.MeshMatcapMaterial({
+        normalMap : branchNormalMap,
+        map : branchMap
+    })
 
     model.scene.traverse((child)=>{
         if(child.name.includes("DOG")){
-            child.material = new THREE.MeshMatcapMaterial({
-                normalMap:textures.normalMap,
-                matcap:textures.sampleMatCap
-            })
+            child.material = dogMaterial
+        }else{
+            child.material = branchMaterial
         }
     })
+
+    const dogModel = useRef(model)
+
+    useGSAP(()=>{
+        const tl = gsap.timeline({
+            scrollTrigger:{
+                trigger:"#section-1",
+                endTrigger:"#section-3",
+                start:"top top",
+                end:"bottom bottom",
+                markers:true,
+                scrub:true,
+            }
+        })
+
+        tl
+        .to(dogModel.current.scene.position,{
+            z:"-=0.75",
+            y:"+=0.1"
+        })
+        .to(dogModel.current.scene.rotation,{
+            x:`+=${Math.PI/15}`
+        })
+        .to(dogModel.current.scene.rotation,{
+            y:`-=${Math.PI}`,
+
+        },"third")
+        .to(dogModel.current.scene.position,{
+            x: "-=0.7",
+            z: "+=0.6",
+            y:"-=0.05",
+        },"third")
+
+    },[])
 
 
   return (
     <>
         <primitive object={model.scene} position={[0.3,-1.04,0]} scale={[2,2,2]} rotation={[0 , Math.PI/4 , 0]} />
         <directionalLight position={[0,5,5]} intensity={10} color={0xFFFFFF} />
-        <OrbitControls/>
-    </>
+   </>
   )
 }
 

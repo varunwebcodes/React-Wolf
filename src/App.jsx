a26c73b8970233d99025417ef80dea08e28989be
varunwebcodes <varunnegiweb>
2026-01-09 23:35:48 +0530

    CSS UPDATED

--- src/App.jsx
+++ src/App.jsx
@@ -7,16 +7,22 @@ function App() {
   return (
     <>
     <main>
-      <Canvas style={{
+      <div className="images">
+        <img id='tomorrowland' src="/tomorrowland.png" alt="" />
+        <img id='navy' src="/navy.png" alt="" />
+        <img id='chicago' src="/chicago.png" alt="" />
+        <img id='phone' src="/phone.png" alt="" />
+        <img id='kikk' src="/kikk.png" alt="" />
+        <img id='kennedy' src="/kennedy.png" alt="" />
+        <img id='opera' src="/opera.png" alt="" />
+      </div>
+      <Canvas id='canvas-elem' style={{
         height:"100vh",
         width:"100vw",
         position:"fixed",
         top:0,
         left:0,
         zIndex:1,
-        backgroundImage:"url(/background-l.png)",
-        backgroundRepeat:"no-repeat",
-        backgroundSize:"cover"
       }}>
         <Dog/>
       </Canvas>
@@ -41,19 +47,64 @@ function App() {
               <path d="M30.4351,61.1758h-10.4155L0,116.838H10.3479L30.4351,61.1758Z"></path>
             </svg>
           </div>
-          <div className='nav-elem'><i className="ri-arrow-right-s-line"></i>Our ShowReel</div>
+          <div className='nav-elem'><i className="ri-arrow-right-s-line"></i>Our Showreel</div>
           <div className='nav-elem'>
             <i className="ri-menu-3-line"></i>
           </div>
         </nav>
         <div className="middle">
           <div className='left'>
-            <h1>WE <br /> Make <br /> Good <br /> Shit</h1>
+            <h1>We <br /> Make <br /> Good <br /> Shit</h1>
           </div>
           <div className='right'></div>
         </div>
+        <div className="bottom">
+          <div className="left"></div>
+          <div className="right">
+            <p>
+              Dogstudio is a multidisciplinary <br />
+              creative studio at the intersection <br />
+              of art, design and technology.
+            </p>
+          </div>
+        </div>
+
+        <div className="first-line"></div>
+        <div className="second-line"></div>
+       </section>
+      <section id='section-2'>
+        <div className="titles">
+          <div img-title="tomorrowland" className="title">
+            <small>2020 - ONGOING</small>
+            <h1>Tomorrowland</h1>
+          </div>
+          <div  img-title="navy" className="title">
+            <small>2018 - TODAY</small>
+            <h1>Navy Pier</h1>
+          </div>
+          <div  img-title="chicago" className="title">
+            <small>2015 - TODAY</small>
+            <h1>MSI Chicago</h1>
+          </div>
+          <div  img-title="phone" className="title">
+            <small>2016</small>
+            <h1>This Was Louise's Phone </h1>
+          </div>
+          <div  img-title="kikk" className="title">
+            <small>2012 - TODAY</small>
+            <h1>KIKK Festival 2018</h1>
+          </div>
+          <div img-title="kennedy" className="title">
+            <small>2017</small>
+            <h1>The Kennedy Center</h1>
+          </div>
+          <div img-title="opera" className="title">
+            <small>2016 - ONGOING</small>
+            <h1>Royal Opera Of Wallonia</h1>
+          </div>
+        </div>
       </section>
-      <section id='section-2' ></section>
+
       <section id='section-3'></section>
     </main>
     </>
